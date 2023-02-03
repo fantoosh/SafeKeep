5ea61018768dadc94da74997e9e67d913538fb19
fantoosh <avalon74@protonmail.com>
2023-02-03 05:20:31 +0100

    modified: views.py

--- app/pgenerator/views.py
+++ app/pgenerator/views.py
@@ -1,20 +1,21 @@
 from flask_login import login_required
-from flask import Blueprint, flash ,redirect, render_template, request, url_for
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 from . import pgenerator
 import random
 import string
 
-@pgenerator.route('/pgenerator', methods=['GET', 'POST'])
-def pgenerate(): 
-    #default value of Filters
+
+@pgenerator.route("/pgenerator", methods=["GET", "POST"])
+def pgenerate():
+    # default value of Filters
     asciiFilter = False
     lowerFilter = False
     upperFilter = False
     puncFilter = False
     digitsFilter = False
-    
+
     if request.method == "POST":
-        
+
         if not request.form.get("asciiF"):
             asciiFilter = True
         if not request.form.get("lowerF"):
@@ -25,27 +26,31 @@ def pgenerate():
             puncFilter = True
         if not request.form.get("digitsF"):
             digitsFilter = True
-            
+
         length = int(request.form.get("length"))
-        
+
         if length >= 8 and length <= 40:
-            password = pgen(length,asciiFilter,lowerFilter,upperFilter,puncFilter,digitsFilter)
-            
-            return render_template('pgenerator.html',password=password)
+            password = pgen(
+                length, asciiFilter, lowerFilter, upperFilter, puncFilter, digitsFilter
+            )
 
-    
-    password = pgen(int(13),asciiFilter,lowerFilter,upperFilter,puncFilter,digitsFilter)
-    return render_template('pgenerator/pgenerator.html',password=password)  #set 13 as the default length of generated password
+            return render_template("pgenerator.html", password=password)
+
+    password = pgen(
+        int(13), asciiFilter, lowerFilter, upperFilter, puncFilter, digitsFilter
+    )
+    return render_template(
+        "pgenerator/pgenerator.html", password=password
+    )  # set 13 as the default length of generated password
 
 
+def pgen(length, asciiFilter, lowerFilter, upperFilter, puncFilter, digitsFilter):
 
-def pgen(length,asciiFilter,lowerFilter,upperFilter,puncFilter,digitsFilter):
- 
-    password =''
-    #All usable characters
+    password = ""
+    # All usable characters
     chars = string.ascii_letters + string.digits + "!@#$%^&*?"
-    #Add selected filters into the filter list:
-    filter = ''
+    # Add selected filters into the filter list:
+    filter = ""
     if asciiFilter == True:
         filter += string.ascii_letters
     if lowerFilter == True:
@@ -56,21 +61,19 @@ def pgen(length,asciiFilter,lowerFilter,upperFilter,puncFilter,digitsFilter):
         filter += "!@#$%^&*?"
     if digitsFilter == True:
         filter += string.digits
-    
-    
-    #maketrans to create a mapping for removing the filtered characters inside chars
-    filters = chars.maketrans('', '', filter)
-    #translate to use the mapping
+
+    # maketrans to create a mapping for removing the filtered characters inside chars
+    filters = chars.maketrans("", "", filter)
+    # translate to use the mapping
     char_seq = chars.translate(filters)
-    
-    
-    #create random chars for the selected length
+
+    # create random chars for the selected length
     for len in range(int(length)):
         random_char = random.choice(char_seq)
         password += random_char
-            
-        #reshuffle the list of chars in the generated password 
+
+        # reshuffle the list of chars in the generated password
         passwordlist = list(password)
         random.shuffle(passwordlist)
-        final_password = ''.join(passwordlist)
+        final_password = "".join(passwordlist)
     return final_password
