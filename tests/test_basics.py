445d04bf52bbf07a9201461556c8abc1a777eaee
fantoosh <avalon74@protonmail.com>
2023-05-03 06:55:27 +0100

    modified:   tests/test_basics.py 	modified:   .idea/password-manager.iml

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -6,7 +6,7 @@ from app.models import User
 
 class BasicsTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = create_app('testing')
+        self.app = create_app("testing")
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
@@ -20,25 +20,25 @@ class BasicsTestCase(unittest.TestCase):
         self.assertFalse(current_app is None)
 
     def test_app_is_testing(self):
-        self.assertTrue(current_app.config['TESTING'])
+        self.assertTrue(current_app.config["TESTING"])
 
 
 class UserModelTestCase(unittest.TestCase):
     def test_password_setter(self):
-        u = User(password = 'cat')
+        u = User(password="cat")
         self.assertTrue(u.password_hash is not None)
 
     def test_no_password_getter(self):
-        u = User(password = 'cat')
+        u = User(password="cat")
         with self.assertRaises(AttributeError):
             u.password
-            
+
     def test_password_verification(self):
-        u = User(password = 'cat')
-        self.assertTrue(u.verify_password('cat'))
-        self.assertFalse(u.verify_password('dog'))
+        u = User(password="cat")
+        self.assertTrue(u.verify_password("cat"))
+        self.assertFalse(u.verify_password("dog"))
 
     def test_password_salts_are_random(self):
-        u = User(password='cat')
-        u2 = User(password='cat')
+        u = User(password="cat")
+        u2 = User(password="cat")
         self.assertTrue(u.password_hash != u2.password_hash)
